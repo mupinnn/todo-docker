7603e2023b5eca33c815886ed99153ae43783983
Ahmad Muwaffaq <itsmupin@gmail.com>
2026-02-21 23:39:43 +0700

    chore: db migration using `dbmate`

---
+++ apps/api/db/migrations/20260221162310_create_users_table.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+CREATE TABLE users (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  email varchar(255) NOT NULL UNIQUE,
+  password text NOT NULL,
+  refresh_token text,
+  created_at timestamptz DEFAULT timezone('utc'::text, now()) NOT NULL,
+  updated_at timestamptz
+);
+
+-- migrate:down
+DROP TABLE users;
--- apps/api/sql/rollback.sql
+++
@@ -1,4 +0,0 @@
--- rollback with `psql -d todo-docker -f api/sql/schema.sql`
-
-drop table if exists todos cascade;
-drop table if exists users cascade;
