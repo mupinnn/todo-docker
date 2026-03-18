99e0d75f816c26c17dc3cb1643a06080d38d7917
Ahmad Muwaffaq <itsmupin@gmail.com>
2026-03-18 21:14:36 +0700

    feat(web): sessions ui

--- apps/web/src/routes/_auth.profile.tsx
+++ apps/web/src/routes/_auth.profile.tsx
@@ -1,5 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useAuth } from "@/components/providers/auth-provider";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_auth/profile")({
   component: RouteComponent,
@@ -11,5 +20,30 @@ export const Route = createFileRoute("/_auth/profile")({
 function RouteComponent() {
   const { profile } = useAuth();
 
-  return <div>Hello, {profile?.email}!</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Hello, {profile?.email}!</CardTitle>
+          <CardDescription>
+            Places where you're logged in into To-Do.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Chrome</CardTitle>
+              <CardDescription className="flex items-center gap-2">
+                <span className="flex size-2 rounded-full bg-green-500" />
+                Current session
+              </CardDescription>
+              <CardAction>
+                <Button variant="outline">Sign out</Button>
+              </CardAction>
+            </CardHeader>
+          </Card>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
