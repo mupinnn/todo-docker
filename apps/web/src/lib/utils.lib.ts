947a11cc216ae5f8cecbd4d10fe3d0b38fce99ac
Ahmad Muwaffaq <itsmupin@gmail.com>
2026-03-20 14:17:08 +0700

    feat(web): dynamic env

--- apps/web/src/lib/utils.lib.ts
+++ apps/web/src/lib/utils.lib.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getEnv<K extends keyof ImportMetaEnv>(key: K) {
+  const envMap = {
+    BASE_URL: import.meta.env.BASE_URL,
+    DEV: import.meta.env.DEV,
+    MODE: import.meta.env.MODE,
+    PROD: import.meta.env.PROD,
+    SSR: import.meta.env.SSR,
+    VITE_API_URL: import.meta.env.VITE_API_URL,
+  } satisfies ImportMetaEnv;
+
+  return window?.env?.[key] ?? envMap[key];
+}
